463c457cb75c66b2876df14e8a21f5ce7b7e9c53
fork_IVK <ivkor72@mail.ru>
2024-06-20 14:19:46 +0300

    1

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,34 @@
 package jm.task.core.jdbc.util;
+//import java.sql.Connection;
+//import java.sql.Driver;
+//import java.sql.DriverManager;
+import java.sql.*;
+
+
+
+
+import java.sql.SQLException;
 
 public class Util {
     // реализуйте настройку соеденения с БД
-}
+    private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/users";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "IVK_student_kata-71";
+
+    Connection connection;
+
+    public Connection getConnection(){
+
+        try {
+            Class.forName(DB_DRIVER);
+            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+            System.out.println("Connection is successful");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+           // System.out.println("Connection failed");
+        }
+    return connection;
+    }
+
+} //util
