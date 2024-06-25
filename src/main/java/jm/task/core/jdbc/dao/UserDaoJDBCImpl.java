83a58b22d00785c5d7bfdb381f78e9b09633d3f1
fork_IVK <ivkor72@mail.ru>
2024-06-25 05:35:43 +0300

    3

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -3,12 +3,11 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
-public class UserDaoJDBCImpl implements UserDao {
+public class UserDaoJDBCImpl extends Util implements UserDao {
     public UserDaoJDBCImpl() {
 
     }
@@ -24,12 +23,12 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void saveUser(String name, String lastName, byte age) throws SQLException {
-        PreparedStatement preparedStatement = null;
+
 
         String sql = "INSERT INTO users_table (id, name, lastname, age) VALUES (default, ?, ?, ?)";
 
-        preparedStatement.getConnection();
-        try (preparedStatement) {
+        ;
+        try (Connection connection = getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setString(2, name);
             preparedStatement.setString(3, lastName);
             preparedStatement.setByte(4, age);
@@ -42,12 +41,38 @@ public class UserDaoJDBCImpl implements UserDao {
 
 
     }
-    public void removeUserById(long id) {
+    public void removeUserById(long id) throws SQLException {
+        String sql = "DELETE FROM users_table WHERE id = ?";
 
+        try (Connection connection = getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
-    public List<User> getAllUsers() {
-        return null;
+    public List<User> getAllUsers() throws SQLException {
+        List <User> userList = new ArrayList<>();
+        String sql = "SELECT id, name, lastName, age FROM users_table";
+
+        try (Connection connection = getConnection();Statement statement = connection.createStatement()){
+
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()){
+                User user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setName(resultSet.getString("lastName"));
+                user.setAge(resultSet.getByte("age"));
+                userList.add(user);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return userList;
     }
 
     public void cleanUsersTable() {
