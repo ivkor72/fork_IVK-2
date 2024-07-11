4ea4c05ab0baf240bb549349c00cfc88e961204c
ivkor72 <ivkor72@mail.ru>
2024-07-11 11:08:02 +0300

    finally for task 1.1.4

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -19,13 +19,13 @@ public class Main {
         userDaoJDBC.createUsersTable();
 
         userDaoJDBC.saveUser("Sergey", "Sergeev", (byte) 29);
-        System.out.println("User for name 'Sergey Sergeev' added to the database");
+        System.out.println("User for name 'Sergey' added to the database");
         userDaoJDBC.saveUser("Ivan", "Petrov", (byte) 32);
-        System.out.println("User for name 'Ivan Petrov' added to the database");
+        System.out.println("User for name 'Ivan' added to the database");
         userDaoJDBC.saveUser("Igor", "Sidorov", (byte) 30);
-        System.out.println("User for name 'Igor Sidorov' added to the database");
+        System.out.println("User for name 'Igor' added to the database");
         userDaoJDBC.saveUser("Dmitriy", "Ivanov", (byte) 25);
-        System.out.println("User for name 'Dmitriy Ivanov' added to the database");
+        System.out.println("User for name 'Dmitriy' added to the database");
 
         List<User> userList = userDaoJDBC.getAllUsers();
         System.out.println(userList);
